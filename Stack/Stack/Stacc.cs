2b0fd9e1c624ae80601aa6c9a42ef7b72155e42b
Paresh Kacha <paresh_kacha48@rocketmail.com>
2019-12-12 22:53:32 +1300

    .
    .

---
+++ Stack/Stack/Stacc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stacc<T>
+    {
+        #region Members
+        private T[] stackArray;
+        private int maxLength;
+        #endregion
+
+        #region Properties
+        public int Size { get; private set; }
+        #endregion
+
+        #region Construtor
+        public Stacc(int length)
+        {
+            maxLength = length;
+            stackArray = new T[length];
+        }
+        #endregion
+
+        public void Push(T value)
+        {
+            if (Size == maxLength)
+            {
+                throw new ExceededSizeException();
+            }
+            stackArray[Size++] = value;
+        }
+
+        public T Pop()
+        {
+            if(Size == 0)
+            {
+                throw new ExpanditureProhibitionException();
+            }
+            return stackArray[--Size];
+        }
+
+        public T Peek()
+        {
+            if (Size == 0)
+            {
+                throw new ExpanditureProhibitionException();
+            }
+
+            return stackArray[Size - 1];
+        }
+    }
+}
---
+++ Stack/StackTests/StackTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Stack;
+
+namespace StackTests
+{
+    [TestFixture]
+    public class StackTest
+    {
+        [Test]
+        public void Creation()
+        {
+            Stacc<int> s = new Stacc<int>(3);
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            int value = s.Pop();
+            Assert.AreEqual(3, value);
+            Assert.AreEqual(2, s.Size);
+        }
+
+        [Test]
+        public void Too_Much_Pop()
+        {
+            Stacc<int> s = new Stacc<int>(3);
+            Assert.Throws<ExpanditureProhibitionException>(() => s.Pop());
+        }
+
+        [Test]
+        public void Too_Much_Push()
+        {
+            Stacc<int> s = new Stacc<int>(3);
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            Assert.Throws<ExceededSizeException>(() => s.Push(4));
+        }
+
+        [Test]
+        public void Test_Peek_Element()
+        {
+            Stacc<int> s = new Stacc<int>(3);
+            s.Push(2);
+            s.Push(1);
+            int value = s.Peek();
+            Assert.AreEqual(1, value);
+            Assert.AreEqual(2, s.Size);
+        }
+
+        [Test]
+        public void Test_Peek_Eeception()
+        {
+            Stacc<int> s = new Stacc<int>(3);
+            Assert.Throws<ExpanditureProhibitionException>(() => s.Peek());
+        }
+    }
+}
